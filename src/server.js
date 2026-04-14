62af1183b1bdbd976950457d321a06830e2533a5
lfcgame <lfclive@163.com>
2026-04-14 21:01:03 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,56 +1,23 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const Router = require('@koa/router');
-const axios = require('axios');
+const Koa = require('koa')
+const Router = require('@koa/router')
+const bodyParser = require('koa-bodyparser')
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
 
-router.get('/', (ctx) => {
-  ctx.body = "Nodejs koa demo project";
-});
+app.use(bodyParser())
 
-router.get('/api/get_open_id', async (ctx) => {
-  const value = ctx.request.header['x-tt-openid'];
-  if (value) {
-    ctx.body = {
-      success: true,
-      data: value
-    };
-  } else {
-    ctx.body = {
-      success: false,
-      message: "dyc-open-id not exist"
-    };
-  }
-});
+router.get('/', ctx => {
+  ctx.body = '服务正常运行'
+})
 
-router.post('/api/text/antidirt', async (ctx) => {
-  const body = ctx.request.body;
-  const content = body.content;
-  const res = await axios.post('http://developer.toutiao.com/api/v2/tags/text/antidirt', {
-    "tasks": [
-      { "content": content }
-    ]
-  });
-  ctx.body = {
-    "result": res.data,
-    "success": true
-  };
-});
+router.get('/api/hello', ctx => {
+  ctx.body = { success: true, msg: '接口通了！' }
+})
 
-// 你的测试接口
-router.get('/api/hello', async (ctx) => {
-  ctx.body = {
-    success: true,
-    msg: "柚趣互娱测试接口成功！"
-  };
-});
+app.use(router.routes())
 
-app.use(bodyParser());
-app.use(router.routes());
-
-const PORT = 8000;
+const PORT = 8000
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log('started on 8000')
+})
