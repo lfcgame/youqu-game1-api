7f6287e6b14e3268a4d037dc86499d87ab010a1b
lfcgame <lfclive@163.com>
2026-04-14 21:30:51 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,7 @@
-const Koa = require('koa')
-const Router = require('@koa/router')
-const bodyParser = require('koa-bodyparser')
-
-const app = new Koa()
-const router = new Router()
-
-app.use(bodyParser())
-
-router.get('/', ctx => {
-  ctx.body = '服务正常运行'
-})
-
-router.get('/api/hello', ctx => {
-  ctx.body = { success: true, msg: '接口通了！' }
-})
-
-app.use(router.routes())
-
-const PORT = 8000
-app.listen(PORT, () => {
-  console.log('started on 8000')
-})
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Server is running');
+});
+server.listen(8000, () => {
+  console.log('Listen on 8000');
+});
